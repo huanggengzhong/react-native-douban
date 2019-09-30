3e47dad19bb1c96001be05c7a374d3392c421b7c
huanggengzhong <915835528@qq.com>
2019-09-30 16:00:57 +0800

    完成了轮播图

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,10 +1,88 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Dimensions, StyleSheet, Image} from 'react-native';
+import Carousel from 'react-native-looped-carousel';
+
+const {width} = Dimensions.get('window');
+const styles = StyleSheet.create({
+  images: {
+    width: '100%',
+    height: '100%',
+  },
+});
 export default class Home extends Component {
+  state = {
+    size: {width, height: 200},
+  };
   render() {
     return (
       <View>
-        <Text>this is Home</Text>
+        {/* 1.自定义导航条 */}
+        
+        {/* 2.轮播图 */}
+        <View style={this.state.size} onLayout={this._onLayoutDidChange}>
+          <Carousel
+            delay={3000}
+            style={this.state.size}
+            autoplay
+            // pageInfo
+            arrows={true}
+            leftArrowText="<"
+            leftArrowStyle={{color: 'white', fontSize: 30, margin: 20}}
+            rightArrowStyle={{color: 'white', fontSize: 30, margin: 20}}
+            rightArrowText=">"
+            bullets
+            onAnimateNextPage={p => console.log(p)}>
+            <View style={this.state.size}>
+              <Image
+                style={styles.images}
+                resizeMode="stretch"
+                source={{
+                  uri:
+                    'https://img3.mukewang.com/szimg/5d4ceaef09c3cb6612000676-360-202.png',
+                }}
+              />
+            </View>
+            <View style={this.state.size}>
+              <Image
+                style={styles.images}
+                resizeMode="stretch"
+                source={{
+                  uri:
+                    'https://img4.mukewang.com/szimg/5ad05dc00001eae705400300-360-202.jpg',
+                }}
+              />
+            </View>
+            <View style={this.state.size}>
+              <Image
+                style={styles.images}
+                resizeMode="stretch"
+                source={{
+                  uri:
+                    'https://img1.mukewang.com/szimg/5d665288090f60db12000676-360-202.png',
+                }}
+              />
+            </View>
+          </Carousel>
+        </View>
+        {/* ====================下面轮播图已经有效果================ */}
+        {/* <View style={this.state.size} onLayout={this._onLayoutDidChange}>
+          <Carousel
+            delay={3000}
+            style={this.state.size}
+            autoplay
+            pageInfo
+            onAnimateNextPage={p => console.log(p)}>
+            <View style={[{backgroundColor: '#BADA55'}, this.state.size]}>
+              <Text>1</Text>
+            </View>
+            <View style={[{backgroundColor: 'red'}, this.state.size]}>
+              <Text>2</Text>
+            </View>
+            <View style={[{backgroundColor: 'blue'}, this.state.size]}>
+              <Text>3</Text>
+            </View>
+          </Carousel>
+        </View> */}
       </View>
     );
   }
